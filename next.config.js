acb72fea81982318dc819296285c705aa7d105b4
Erick Bazán <erick.bazan@hey.com>
2023-08-26 00:40:09 -0600

    🪴 Setup Docs for SingaKBD

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
 
-module.exports = nextConfig
+module.exports = withNextra({
+  reactStrictMode: true,
+  webpack(config) {
+    const allowedSvgRegex = /components\/icons\/.+\.svg$/;
+
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test(".svg"),
+    );
+    fileLoaderRule.exclude = allowedSvgRegex;
+
+    config.module.rules.push({
+      test: allowedSvgRegex,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+});
