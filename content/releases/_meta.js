36a6c28e999d32d810e7924c63a642a5497a5899
Erick Bazán <erick.bazan@hey.com>
2026-04-09 16:54:15 -0600

    feat: Add Nextra Cards, Tabs, and frontmatter across all pages (#31)
    - Add Cards grid on home page for release overview
    - Add Tabs for layout images on multi-version pages (Jaguar, Kohaku, Ocelot)
    - Add frontmatter descriptions to all MDX pages for SEO
    - Remove emojis from sidebar and cards

--- content/releases/_meta.js
+++ content/releases/_meta.js
@@ -1,9 +1,9 @@
 export default {
-  jaguar: "🐆 Jaguar",
-  kohaku: "🐟 Kohaku",
-  neko: "🐈 Neko (w. MONOKEI)",
-  ocelot: "🐈‍⬛ Ocelot",
-  singa: "🦁 SINGA",
-  unikorn: "🦄 Unikorn (w. TGR)",
+  jaguar: "Jaguar",
+  kohaku: "Kohaku",
+  neko: "Neko (w. MONOKEI)",
+  ocelot: "Ocelot",
+  singa: "SINGA",
+  unikorn: "Unikorn (w. TGR)",
   stinga: "Stinga",
 };
